2756dc6db765479e25bd63e5034241f3ec773a9c
Kacper Sopata <kacper_sopata@icloud.com>
2025-06-01 16:21:52 +0200

    Update

--- ECommerce/Program.cs
+++ ECommerce/Program.cs
@@ -1,24 +1,22 @@
-using ECommerce.Application;
+﻿using ECommerce.Application;
 using ECommerce.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-
-// Rejestracja DI aplikacji i infrastruktury
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ECommerceDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+// 👇 DODAJ TO
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 var app = builder.Build();
 
@@ -28,10 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
